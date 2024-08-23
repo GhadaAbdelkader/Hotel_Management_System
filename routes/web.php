87a82774aab0aee6e91499be17bfae23cb7b0602
Ghada Abdelkader <ghada.abdo410@gmail.com>
2024-08-23 22:32:35 +0300

    admin for admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminPanal\AmenityController;
 use App\Http\Controllers\AdminPanal\RegisterController;
 use App\Http\Controllers\AdminPanal\RoomController;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,12 @@ Route::get('/', function () {
 });
 
 Route::resource('/admin/rooms', RoomController::class);
+
+//Route::resource('/admin/amenities', AmenityController::class);
+Route::middleware(['web'])->group(function () {
+    Route::resource('/admin/amenities', AmenityController::class);
+    // باقي الراوتات
+});
 Route::middleware(['web'])->group(function () {
     Route::get('register', [RegisterController::class, 'create']);
     Route::post('register', [RegisterController::class, 'store']);});
